2b403a2534b3cffd83219b6eb0ef15b7f8fa6ff5
Florian <contact@empreintesdigitales.fr>
2025-03-22 15:44:26 +0100

    first

---
+++ projet/frontend/src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import Header from './Header/Header';
+
+import './App.scss';
+import Footer from './Footer/Footer';
+
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
